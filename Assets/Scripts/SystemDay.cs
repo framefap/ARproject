b9aba221cde4f4bf569abbe917a2063670d3793c
WinAndBadger <borwornsak456@gmail.com>
2021-02-22 11:37:46 +0700

    First Committ

---
+++ Assets/Scripts/SystemDay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemDay : MonoBehaviour
+{
+    public TextMeshProUGUI dateText;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        string day = System.DateTime.Now.ToString("d");
+
+        dateText.text = string.Format(day);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string day = System.DateTime.Now.ToString("d");
+        
+        dateText.text = string.Format(day);
+    }
+}

ba4a2bd50fcbe635ed34d31b8530edfaa40f91f8
WinAndBadger <borwornsak456@gmail.com>
2021-02-28 22:48:24 +0700

    Update

---
+++ Assets/Scripts/SystemDay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemDay : MonoBehaviour
+{
+    CultureInfo ci = new CultureInfo("en-US");
+    public TextMeshProUGUI dateText;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        string day = System.DateTime.Now.ToString("ddd , dd MMM yy",ci);
+
+        dateText.text = string.Format(day);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string day = System.DateTime.Now.ToString("ddd , dd MMM yy", ci);
+        
+        dateText.text = string.Format(day);
+    }
+}

2c2f5aff5939493aa04a7969c6818a06d543dadc
framefap <frame-kaiser@hotmail.com>
2021-03-25 12:02:25 +0700

    latest update

---
+++ Assets/SDK/Modules/Module_AndroidPermission/Scripts/PermissionRequest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PermissionRequest : MonoBehaviour
+{
+
+    public bool Camera = true;
+    public bool ExternalStorageRead = true;
+    public bool ExternalStorageWrite = true;
+    public bool Microphone = false;
+    public bool FineLocation = false;
+    public bool CoarseLocation = false;
+
+    void Awake() {
+        List<string> permissionList = new List<string>();
+
+        if(Camera && !Permission.HasUserAuthorizedPermission(Permission.Camera)) {
+            permissionList.Add(Permission.Camera);
+        }
+        if(ExternalStorageRead && !Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead)) {
+            permissionList.Add(Permission.ExternalStorageRead);
+        }
+        if(ExternalStorageWrite && !Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite)) {
+            permissionList.Add(Permission.ExternalStorageWrite);
+        }
+        if(Microphone && !Permission.HasUserAuthorizedPermission(Permission.Microphone)) {
+            permissionList.Add(Permission.Microphone);
+        }
+        if(FineLocation && !Permission.HasUserAuthorizedPermission(Permission.FineLocation)) {
+            permissionList.Add(Permission.FineLocation);
+        }
+        if(CoarseLocation && !Permission.HasUserAuthorizedPermission(Permission.CoarseLocation)) {
+            permissionList.Add(Permission.CoarseLocation);
+        }
+
+        if(permissionList.Count > 0) {
+            AndroidPermission.getInstant.GetPermission(permissionList);
+        }
+    }
+
+
+
+}
